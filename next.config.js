379850ff71b2f15330887152fead97a76bda9222
kaleido01 <47937165+kaleido01@users.noreply.github.com>
2019-09-25 23:59:49 +0900

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// must restart server whenever you make changes in next.config
-module.exports = {
-  env: {
-    MONGO_SRV: 'mongodb://localhost:27017/react-reserve',
-    JWT_SECRET: '<insert-jwt-secret>',
-    CLOUDINARY_URL: '<insert-cloudinary-url>',
-    STRIPE_SECRET_KEY: '<insert-stripe-secret-key>',
-  },
-}
