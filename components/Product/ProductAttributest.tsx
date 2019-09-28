5568c560c0d39fbba4f5de2d94d3637a9eb93604
kaleido01 <mbdkaleido@gmail.com>
2019-09-28 11:59:36 +0900

    add delete operation

--- components/Product/ProductAttributest.tsx
+++ components/Product/ProductAttributest.tsx
@@ -1,16 +1,49 @@
 import React from 'react'
 import { ProductType } from '../../models/Product'
-import { Header, Button } from 'semantic-ui-react'
+import { Header, Button, Modal } from 'semantic-ui-react'
+import axios from 'axios'
+import baseUrl from '../../utils/baseUrl'
+import { useRouter } from 'next/router'
 type Props = {
   product: ProductType
 }
 const ProductAttributest = ({ product }: Props) => {
-  const { description } = product
+  const [open, setOpen] = React.useState(false)
+  const { description, _id } = product
+  const router = useRouter()
+
+  const handleDelete = async () => {
+    const payload = { params: { _id } }
+    const url = `${baseUrl}/api/product`
+    await axios.delete(url, payload)
+    router.push('/')
+  }
   return (
     <>
       <Header as="h3">About this product</Header>
       <p>{description}</p>
-      <Button icon="trash alternate outline" color="red" content="Delete Product" />
+      <Button
+        icon="trash alternate outline"
+        color="red"
+        content="Delete Product"
+        onClick={() => setOpen(true)}
+      />
+      <Modal open={open} dimmer={'blurring'} style={{ top: '38%' }}>
+        <Modal.Header>Confirm Delete</Modal.Header>
+        <Modal.Content>
+          <p>Are you sure you want to delete this product?</p>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button content="Cancel" onClick={() => setOpen(false)} />
+          <Button
+            negative
+            icon="trash"
+            labelPosition="right"
+            content="Delete"
+            onClick={handleDelete}
+          />
+        </Modal.Actions>
+      </Modal>
     </>
   )
 }
--- pages/api/product.ts
+++ pages/api/product.ts
@@ -4,8 +4,34 @@ import connectDb from '../../utils/connectDb'
 
 connectDb()
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+const handleGetRequest = async (req: NextApiRequest, res: NextApiResponse) => {
   const { _id } = req.query
   const product = await Product.findOne({ _id })
   res.status(200).json(product)
 }
+const handleDeleteRequest = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { _id } = req.query
+  await Product.deleteOne({ _id })
+  res.status(204).json({})
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  switch (req.method) {
+    case 'GET':
+      await handleGetRequest(req, res)
+      break
+    case 'DELETE':
+      await handleDeleteRequest(req, res)
+      break
+
+    default:
+      res.status(405).send(`Method ${req.method} not allowed`)
+      break
+  }
+}
+
+// export default async (req: NextApiRequest, res: NextApiResponse) => {
+//   const { _id } = req.query
+//   const product = await Product.findOne({ _id })
+//   res.status(200).json(product)
+// }
