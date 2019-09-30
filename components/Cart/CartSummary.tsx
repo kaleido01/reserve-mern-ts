c9015ba391b2de40927c6267f382b0bb0a672bc2
kaleido01 <mbdkaleido@gmail.com>
2019-09-30 13:54:33 +0900

    fix lint

--- components/Cart/CartSummary.tsx
+++ components/Cart/CartSummary.tsx
@@ -7,7 +7,7 @@ const CartSummary = () => {
       <Divider />
       <Segment clearing size="large">
         <strong>Sub total:</strong> $ 0.00
-        <Button icon="cart" color="teal" floated={'right'} content="Checkout" />>
+        <Button icon="cart" color="teal" floated={'right'} content="Checkout" />
       </Segment>
     </>
   )
